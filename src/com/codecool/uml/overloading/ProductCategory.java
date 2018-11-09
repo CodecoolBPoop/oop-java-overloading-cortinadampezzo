91d77402b27c4b1c857c2132e0e456e9652dbcce
cortinadampezzo <kisgans@gmail.com>
2018-11-09 09:48:58 +0100

    skeleton created

---
+++ src/com/codecool/uml/overloading/ProductCategory.java
@@ -0,0 +1,59 @@
+package com.codecool.uml.overloading;
+
+import java.util.List;
+
+public class ProductCategory {
+
+    private static int counter = 0;
+    private int id;
+    private String name;
+    private String department;
+    private String description;
+
+    public ProductCategory() {
+        this.id = counter++;
+    }
+
+    public ProductCategory(String name, String department, String description) {
+        this.name = name;
+        this.department = department;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Product> getProducts() {
+        return Product.getAllProductsBy(this);
+    }
+
+    public String toString() {
+        return "";
+    }
+
+}
